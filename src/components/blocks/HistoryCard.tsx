24bb3d39b6ee6c342fec9ceccfb6c11f4a0cd230
Rei Yasuda <rei.yasuda@outlook.jp>
2025-06-01 20:58:29 +0900

    show missing children data

--- src/components/blocks/HistoryCard.tsx
+++ src/components/blocks/HistoryCard.tsx
@@ -14,6 +14,36 @@ export const HistoryCard: React.FC<HistoryCardProps> = ({ historyCard }) => {
   };
   return (
     <li className="relative shadow-md bg-[var(--color-background)] rounded-b-3xl rounded-tr-3xl p-6 md:p-10 [&:not(:last-child)]:mb-10 md:[&:not(:last-child)]:mb-15">
+      {historyCard.children &&
+        historyCard.children.map((child) => {
+          return (
+            <div
+              className="bg-gradient-to-t from-white to-[var(--color-background)] rounded-2xl p-6 md:p-10 mb-4 md:mb-10"
+              key={child.id}
+            >
+              <div className="flex justify-between mb-2 md:mb-3">
+                <span className="block font-semibold text-lg">{child.title}</span>
+                <span
+                  className={`block py-1 px-2 rounded-4xl ${child.status === '現在' ? 'text-[var(--color-flamingo)] bg-[var(--color-flamingo)]/20' : 'text-[var(--color-dark)] bg-[var(--color-dark)]/20'}`}
+                >
+                  {child.status}
+                </span>
+              </div>
+              <span className="block text-base mb-2 md:mb-3 text-[var(--color-dark)]/70">
+                {child.company}
+                <span className="block md:inline">
+                  （<time dateTime={`${child.period.start}`}>{formatDate(child.period.start)}</time>{' '}
+                  -{' '}
+                  <time dateTime={`${child.period.end}` ? child.period.end : '現在'}>
+                    {child.period.end ? formatDate(child.period.end) : '現在'}
+                  </time>
+                  ）
+                </span>
+              </span>
+              <p>{child.description}</p>
+            </div>
+          );
+        })}
       <div className="bg-white rounded-2xl p-6 md:p-10">
         <span className="absolute -top-1 -left-1 bg-[var(--color-flamingo)] w-5 h-5 rounded-full"></span>
         <div className="flex justify-between mb-2 md:mb-3">
